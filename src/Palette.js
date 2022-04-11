a3cd7ee5134e6292060a7855c12a6b226bf5e497
leoninela <81444895+leonine2021@users.noreply.github.com>
2022-04-11 17:08:57 -0400

    add material-ui dropdown menu

--- src/Palette.js
+++ src/Palette.js
@@ -1,43 +1,44 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
-import "rc-slider/assets/index.css";
+import NavBar from "./NavBar";
 import "./Palette.css";
-import Slider from "rc-slider";
 
 export default class PALETTE extends Component {
   constructor(props) {
     super(props);
-    this.state = { level: 500 };
+    this.state = { level: 500, format: "hex" };
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
   }
 
   changeLevel(level) {
     this.setState({ level });
   }
+  changeFormat(val) {
+    this.setState({ format: val });
+  }
 
   render() {
     const { colors } = this.props.palette;
-    const { level } = this.state;
+    const { level, format } = this.state;
 
     const colorBoxes = colors[level].map((color) => {
       return (
-        <ColorBox background={color.hex} name={color.name} key={color.name} />
+        <ColorBox
+          background={color[format]}
+          name={color.name}
+          key={color.name}
+        />
       );
     });
     return (
       <div className="Palette">
         {/* navbar goes here */}
-        <div className="slider">
-          <Slider
-            number={500}
-            defaultValue={level}
-            min={100}
-            max={900}
-            step={100}
-            onAfterChange={this.changeLevel}
-          />
-        </div>
-
+        <NavBar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+        />
         <div className="Palette-colors">{colorBoxes}</div>
         {/* footer goes here */}
       </div>
