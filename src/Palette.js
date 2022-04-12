f38398beda00bf8bb959e1edaa634fdc9f29680f
leoninela <81444895+leonine2021@users.noreply.github.com>
2022-04-11 21:32:46 -0400

    add snackbar footer

--- src/Palette.js
+++ src/Palette.js
@@ -19,28 +19,26 @@ export default class PALETTE extends Component {
   }
 
   render() {
-    const { colors } = this.props.palette;
+    const { colors, paletteName, emoji } = this.props.palette;
     const { level, format } = this.state;
 
     const colorBoxes = colors[level].map((color) => {
       return (
-        <ColorBox
-          background={color[format]}
-          name={color.name}
-          key={color.name}
-        />
+        <ColorBox background={color[format]} name={color.name} key={color.id} />
       );
     });
     return (
       <div className="Palette">
-        {/* navbar goes here */}
         <NavBar
           level={level}
           changeLevel={this.changeLevel}
           handleChange={this.changeFormat}
         />
         <div className="Palette-colors">{colorBoxes}</div>
-        {/* footer goes here */}
+        <footer className="Palette-footer">
+          {paletteName}
+          <span className="emoji">{emoji}</span>
+        </footer>
       </div>
     );
   }
